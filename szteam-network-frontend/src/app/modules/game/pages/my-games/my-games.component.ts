34f87ae9e1cf95877499fd6aaf3019ac14f0578d
h270916 <h270916@stud.u-szeged.hu>
2025-06-12 18:45:07 +0200

    small fontend change, and user's can now view their own games' reviews

--- szteam-network-frontend/src/app/modules/game/pages/my-games/my-games.component.ts
+++ szteam-network-frontend/src/app/modules/game/pages/my-games/my-games.component.ts
@@ -1,10 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {GameCardComponent} from '../../components/game-card/game-card.component';
-import {NgForOf, NgIf} from '@angular/common';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
 import {PageResponseGameResponse} from '../../../../services/models/page-response-game-response';
 import {GameService} from '../../../../services/services/game.service';
 import {Router, RouterLink} from '@angular/router';
 import {GameResponse} from '../../../../services/models/game-response';
+import {ReviewService} from '../../../../services/services/review.service';
+import {ReviewResponse} from '../../../../services/models/review-response';
+import {FindAllReviewsByGame$Params} from '../../../../services/fn/review/find-all-reviews-by-game';
+import {PageResponseReviewResponse} from '../../../../services/models/page-response-review-response';
 
 @Component({
   selector: 'app-my-games',
@@ -12,7 +16,8 @@ import {GameResponse} from '../../../../services/models/game-response';
     GameCardComponent,
     NgForOf,
     NgIf,
-    RouterLink
+    RouterLink,
+    DatePipe
   ],
   templateUrl: './my-games.component.html',
   standalone: true,
@@ -22,11 +27,16 @@ export class MyGamesComponent implements OnInit {
   gameResponse: PageResponseGameResponse = {};
   protected page: number = 0;
   private size: number = 5;
+  selectedGame: GameResponse | null = null;
+  reviews: ReviewResponse[] = [];
+  currentReviewPage = 0;
+  totalReviewPages = 0;
 
 
   constructor(
     private gameService: GameService,
-    private router: Router
+    private router: Router,
+    private reviewService: ReviewService,
   ) {
   }
 
@@ -96,5 +106,26 @@ export class MyGamesComponent implements OnInit {
 
   editGame(game: GameResponse) {
     this.router.navigate(['games', 'manage', game.id]);
+  }
+
+  onShowDetails(game: GameResponse) {
+    this.selectedGame = game;
+    this.currentReviewPage = 0;
+    this.loadReviews(game.id as number);
+  }
+
+  loadReviews(gameId: number): void {
+    const params: FindAllReviewsByGame$Params = {
+      'game-id': gameId,
+      page: this.currentReviewPage,
+      size: 10
+    };
+    this.reviewService.findAllReviewsByGame(params).subscribe({
+      next: (response: PageResponseReviewResponse) => {
+        this.reviews = response.content || [];
+        this.totalReviewPages = response.totalPages || 0;
+      },
+      error: (err) => console.error('Error loading reviews:', err)
+    });
   }
 }
